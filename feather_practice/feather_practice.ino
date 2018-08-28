56784f804912780eb13b7c6ee10b05bf5ba99d2a
Katey Noone <knoone@mica.edu>
2018-08-28 14:31:36 -0400

    small changes
    edited spacing, added serial print

--- feather_practice/feather_practice.ino
+++ feather_practice/feather_practice.ino
@@ -1,11 +1,14 @@
 
-#define LED 0 //built in LED
-
+/* 
+ *  blink is an easy test to make sure that the information is actually going from computer to board. 
+ *  also -- hello world.
+ */
+ 
+#define LED 0   //built in LED
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
-
 }
 
 void loop() {
@@ -13,5 +16,8 @@ void loop() {
   delay(1000);
   digitalWrite(LED, LOW);
   delay(1000);
-
+  Serial.println("blink");
+  
 }
+
+
